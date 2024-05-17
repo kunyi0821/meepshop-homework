2323b3baaafe8e77b1eb0b06225d822590e7d114
李坤奕(技術部) <kun.li@iwplay.com.tw>
2024-05-17 16:19:32 +0800

    fear:修改原本codeType放出方式

--- controllers/codeType.mjs
+++ controllers/codeType.mjs
@@ -1,16 +1,12 @@
-const codeType = (() =>  {
-    const codeType = {
-        "0000": "Success",
-        "0001": "userName is necessary",
-        "0002": "amount is necessary.",
-        "0003": "Your account balance is insufficient.",
-        "0004": "user is not exist.",
-        "0005": "transferUserId is not exist.",
-        "0006": "Can not transfer to yourself.",
-        "0007": "Transaction failed"
-    };
-
-    return codeType;
-})();
+const codeType = {
+    "0000": "Success",
+    "0001": "userName is necessary",
+    "0002": "amount is necessary.",
+    "0003": "Your account balance is insufficient.",
+    "0004": "user is not exist.",
+    "0005": "transferUserId is not exist.",
+    "0006": "Can not transfer to yourself.",
+    "0007": "Transaction failed"
+};
 
 export default codeType;
